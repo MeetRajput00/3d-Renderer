7952f2daa18b807bc3c912e574a01c9d838000e0
Meet Rajput <rajputmeet10@gmail.com>
2023-07-31 23:30:42 +0530

    header file updated

--- 3d-Renderer/main.cpp
+++ 3d-Renderer/main.cpp
@@ -1,29 +1,26 @@
-#include <iostream>
+#include<iostream>
 
 int main() {
+	//image properties
+	const int image_height = 256;
+	const int image_width = 256;
 
-    // Image
+	std::cout << "PPM image " << image_height << "x" << image_width << '\n';
+	//display image
+	for (int j = image_height - 1; j >= 0; j--) {
+		std::cerr << "Lines remaining " << j << ' '<<std::flush;
+		for (int i = 0; i < image_width; i++) {
+			auto r = double(i) / (image_width - 1);
+			auto g = double(j) / (image_height - 1);
+			auto b = 0.25;
 
-    const int image_width = 256;
-    const int image_height = 256;
+			int ir = static_cast<int>(255.999*r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
 
-    // Render
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = image_height - 1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-        }
-    }
-    std::cerr << "\nDone.\n";
+			std::cout << ir << " " << ig << " " << ib << '\n';
+		}
+	}
+	std::cerr << "Render finished!";
+	return 0;
 }
