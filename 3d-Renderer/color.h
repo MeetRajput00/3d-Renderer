1b20567a0993fa7b0b67b308cb19a85e014d34ad
Meet Rajput <rajputmeet10@gmail.com>
2023-08-01 10:29:21 +0530

    stream using vec3 function added in color.h

---
+++ 3d-Renderer/color.h
@@ -0,0 +1,12 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include"vec3.h";
+#include<iostream>
+
+void write_line(std::ostream& out, color pixel_colors) {
+	out<< static_cast<int>(255.999 * pixel_colors.e[0])<<" "<<static_cast<int>(255.999*pixel_colors.e[1])<<" "<<static_cast<int>(255.999*pixel_colors.e[2])<<'\n';
+}
+
+
+#endif COLOR_H
--- 3d-Renderer/main.cpp
+++ 3d-Renderer/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-
+#include"color.h"
+#include"vec3.h"
 int main() {
 	//image properties
 	const int image_height = 256;
@@ -10,15 +11,8 @@ int main() {
 	for (int j = image_height - 1; j >= 0; j--) {
 		std::cerr << "Lines remaining " << j << ' '<<std::flush;
 		for (int i = 0; i < image_width; i++) {
-			auto r = double(i) / (image_width - 1);
-			auto g = double(j) / (image_height - 1);
-			auto b = 0.25;
-
-			int ir = static_cast<int>(255.999*r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			std::cout << ir << " " << ig << " " << ib << '\n';
+			color pixel_colors(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+			write_line(std::cout, pixel_colors);
 		}
 	}
 	std::cerr << "Render finished!";
